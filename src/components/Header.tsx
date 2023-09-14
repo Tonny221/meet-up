6b687fffc73b3edf863a8e92a5a4ea6c5cbb3931
tonny <anthonny14gabriell@gmail.com>
2023-09-14 15:42:56 -0400

    creating meet-up page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,12 @@ const Header = (props: Props) => {
   return (
     <header className="p-4 flex justify-center">
       <nav className="flex flex-row gap-8">
-        <Link href={"/"}>home</Link>
-        <Link href={"/meet-ups"}>meet ups</Link>
+        <Link href={"/"} className="hover:underline">
+          home
+        </Link>
+        <Link href={"/meet-ups"} className="hover:underline">
+          meet ups
+        </Link>
       </nav>
     </header>
   );
