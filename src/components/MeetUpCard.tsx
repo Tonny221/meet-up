531a6045ad733f8b357cc9668ae6c4bcb7be2714
tonny <anthonny14gabriell@gmail.com>
2023-09-15 19:04:50 -0400

    styling the meetupcard

--- src/components/MeetUpCard.tsx
+++ src/components/MeetUpCard.tsx
@@ -9,7 +9,9 @@ const MeetUpCard = ({ title, descritpion }: Props) => {
   return (
     <section className="border-solid border-black border rounded-xl col-span-1">
       <h1 className="text-center">{title}</h1>
-      <p>{descritpion}</p>
+      <div className="p-4">
+        <p>{descritpion}</p>
+      </div>
     </section>
   );
 };
