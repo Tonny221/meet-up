845000e0641b0a6972544dd09a4e135b43a21cad
tonny <anthonny14gabriell@gmail.com>
2023-09-26 16:21:54 -0400

    changing back to the parent component only

--- src/components/MeetUpForm.tsx
+++ src/components/MeetUpForm.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { Form, FormField } from "./ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
 import { SubmitHandler, useForm } from "react-hook-form";
-import TextInput from "./TextInput";
 import { Button } from "./ui/button";
 import formSchema from "@/utils/meetSchema";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "./ui/input";
 
 const MeetUpForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -19,60 +26,21 @@ const MeetUpForm = () => {
 
   return (
     <Form {...form}>
-      <TextInput
-        control={form.control}
-        label="Title"
-        placeholder="Type the meet up title"
-        name="title"
-      />
-      <TextInput
-        control={form.control}
-        label="Description"
-        placeholder="Type the meet up description"
-        name="description"
-      />
-      <TextInput
-        control={form.control}
-        label="Image"
-        placeholder="Type the meet up image URL"
-        name="image"
-      />
+      <span>{JSON.stringify(form.control._formValues)}</span>
+
       <h1 className="text-xl font-bold my-4">Location</h1>
-      <TextInput
+      <FormField
         control={form.control}
-        label="Country"
-        placeholder="Type the meet up country"
-        name="location"
-      />
-      <TextInput
-        control={form.control}
-        label="State"
-        placeholder="Type the meet up state"
-        name="location"
-      />
-      <TextInput
-        control={form.control}
-        label="District"
-        placeholder="Type the meet up district"
-        name="location"
-      />
-      <TextInput
-        control={form.control}
-        label="Street"
-        placeholder="Type the meet up street"
-        name="location"
-      />
-      <TextInput
-        control={form.control}
-        label="Number"
-        placeholder="Type the meet up number"
-        name="location"
-      />
-      <TextInput
-        control={form.control}
-        label="Complement"
-        placeholder="Type the meet up location complement"
-        name="location"
+        name="location.country"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Country</FormLabel>
+            <FormControl>
+              <Input placeholder="type the meet up country" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
       />
       <Button type="submit">Submit</Button>
     </Form>
